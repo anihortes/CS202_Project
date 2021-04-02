3fe399ff508e62c5771c1ae751adf947021e1da8
Adam Owen <ahowenjr@alaska.edu>
2021-04-01 16:01:21 -0800

    added sfml stuff. DOES NOT COMPILE

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
+#include <vector>
+using std::vector;
 
 int main() {
-    std::cout << "Hello, World" << std::endl;
-    return 0;
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    return EXIT_SUCCESS;
 }
