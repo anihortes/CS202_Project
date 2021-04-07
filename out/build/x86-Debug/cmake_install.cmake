2ee9c29bbfacaf421b86561aad19d6b3c7010a95
William Glass <willg.glass94@gmail.com>
2021-04-06 19:40:35 -0800

    made minor tweaks to CMakeList to run in visual studio. Hope it still works for others.

--- out/build/x86-Debug/cmake_install.cmake
+++ out/build/x86-Debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/willg/OneDrive/CS 202x Project/out/install/x64-Debug (default)")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/willg/OneDrive/CS 202x Project/out/install/x86-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/willg/OneDrive/CS 202x Project/out/build/x64-Debug (default)/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/willg/OneDrive/CS 202x Project/out/build/x86-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
