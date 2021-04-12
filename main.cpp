a49e911be98a3db071660fc7c949e175f5a0f591
Adam Owen <ahowenjr@alaska.edu>
2021-04-11 16:14:52 -0800

    added test class for grid based movement

--- main.cpp
+++ main.cpp
@@ -5,6 +5,35 @@
 #include <vector>
 using std::vector;
 
+sf::RenderWindow myWindow(sf::VideoMode(800,600,32),"title");
+sf::Event ev;
+int tileSize = 50;
+
+class Character{
+public:
+    Character() {
+        x = 0;
+        y = 0;
+        movespeed = 100.0/tileSize;
+
+        for(auto i = 0; i < 4; ++i){
+            move[i] = false;
+            walking = false;
+            myRect.setSize(sf::Vector2f(50,50));
+        }
+    }
+    float x,y,movespeed;
+    void keyMove();
+    void moving();
+
+    enum MOVE{UP,DOWN,RIGHT};
+    bool move[4];
+    bool walking;
+    int nextSpot;
+
+    sf::RectangleShape myRect;
+};
+
 int main() {
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
